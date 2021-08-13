d5e379d3b67facc3501322d109407fa707343aa1
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-13 16:12:04 -0300

    refactor. add insertImage() and makeSearch()

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ form.addEventListener('submit', async function(e){
     e.preventDefault();
     const searchTerm = form.elements.query.value;                                   // Saving input in constant
 
-    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)  // Using STL in URL to fetch the promise
-    .then(res => res.json())                                                        // Parsing the JSON received to a Javascript Object
-    .catch(()=> console.log("Erro"));                                               // Error message if promissed is not resolved
+    // const res = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)  // Using STL in URL to fetch the promise
+    // .then(res => res.json())                                                        // Parsing the JSON received to a Javascript Object
+    // .catch(()=> console.log("Erro"));                                               // Error message if promissed is not resolved
 
-    makeImages(res);                                                                // Calling makeImages();
+    makeSearch(searchTerm);
+
     form.elements.query.value = '';                                                 // Resetting HTML form value to blank;
     
 });
@@ -16,10 +17,25 @@ form.addEventListener('submit', async function(e){
 const makeImages = (shows) => {
     for(let result of shows){
         if(result.show.image){
-            const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
-            document.body.append(img);
+            insertImage(result);
         }
     }
 }
 
+const makeSearch = async function (searchTerm) {
+    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    
+    // Parsing the JSON received to a Javascript Object
+    .then(res => res.json())
+        
+    // Error message if promissed is not resolved                                              
+    .catch(()=> console.log("Erro"));
+    
+    makeImages(res);
+}
+
+const insertImage = (result) => {
+    const img = document.createElement('IMG');
+    img.src = result.show.image.medium;
+    document.body.append(img);
+}
