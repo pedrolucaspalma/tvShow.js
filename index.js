78813d3b09d12ab13af709cda7b57a7e38e419f1
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-13 15:10:23 -0300

    begin async js

---
+++ index.js
@@ -0,0 +1,23 @@
+//Getting user input from form
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+
+    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    .then(res => res.json())
+    .catch(()=> console.log("Erro"));
+
+    // console.log(res[0].show.image.medium)
+
+    const img = document.createElement('IMG');
+    img.src = res[0].show.image.medium;
+
+    
+    // console.log(res[0])
+
+    // setTimeout( () => console.log(response), 5000);
+    
+});
+
